4f96a1d72318b26039a178ed09e539c59243e27d
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-16 06:59:42 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const reviews = [
   },
 ];
 
-// Select items
+// select items
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
@@ -46,47 +46,48 @@ const info = document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('random-btn');
+const randomBtn = document.querySelector('.random-btn');
 
-// Set starting item
+// set starting item
 let currentItem = 0;
 
-// Load initial item
+// load initial item
 window.addEventListener('DOMContentLoaded', function () {
-  showPerson();
-});
-
-// Show person based on item
-function showPerson() {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-}
+});
 
-// Show next person
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+// show next person
 nextBtn.addEventListener('click', function () {
   currentItem++;
   if (currentItem > reviews.length - 1) {
     currentItem = 0;
   }
-  showPerson();
+  showPerson(currentItem);
 });
-
-// Show prev person
+// show prev person
 prevBtn.addEventListener('click', function () {
   currentItem--;
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
   }
-  showPerson();
+  showPerson(currentItem);
 });
-
-// Get the random person
+// show random person
 randomBtn.addEventListener('click', function () {
-  currentItem = Math.floor(Math.random() * reviews.length);
+  console.log('hello');
 
-  console.log(currentItem);
-  showPerson();
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
 });
